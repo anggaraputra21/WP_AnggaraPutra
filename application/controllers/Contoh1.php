f9dd9faf17948da78d421fea054999c867de6ce2
angga <iifan7480@gmail.com>
2023-10-19 23:23:03 +0700

    menambahkan CI3 serta tugas p1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Singgih Anggara Putra
+ Saya tingga di daerah Bumi Sani Permai
+ Hobi yang saya sukai adalah
+ Healing";
+ }
+}
